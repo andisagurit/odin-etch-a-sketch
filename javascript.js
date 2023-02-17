6dc97d8196489c9f3fcc683f910e633cab0a2cc2
Andi <acsagurit27@gmail.com>
2023-02-17 10:59:38 -0600

    Updated files

--- javascript.js
+++ javascript.js
@@ -6,27 +6,32 @@ const resizeGrid = document.querySelector('.resize');
 
 let size = 16;
 let color = 'gray';
+let audio = new Audio("https://www.fesliyanstudios.com/play-mp3/7");
 
-// overall look of the grid
-grid.style.display = 'grid';
-grid.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
-grid.style.setProperty('grid-template-rows', `repeat(${size}, 1fr)`);
-grid.style.border = "3px solid #000000";
-// grid.style.width = "960px";
-// grid.style.overflow = "hidden";
-
-// create grid
+//create grid
 function createGrid(size) {
+  let square = grid.querySelectorAll("div");
+  square.forEach((div) => div.remove());
+
+  // grid.style.display = 'grid';
+  grid.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+  grid.style.setProperty('grid-template-rows', `repeat(${size}, 1fr)`);
+  grid.style.width = "960px";
+  grid.style.overflow = "hidden";
+  grid.style.border = "3px solid";
+  
   for (i = 0; i < (size * size); i++) {
     let square = document.createElement("div");
-    square.style.minHeight = "0";
-    square.style.minWidth = "0";
     square.style.overflow = "hidden";
+    square.style.minHeight = "0"
+    square.style.minWidth = "0"
 
     grid.appendChild(square).className = "grid-item";
     square.addEventListener('mouseover', () => setColor(event));
+    // square.addEventListener('mousedown', () => setColor(event));
   }
 }
+createGrid(size);
 
 function getRandomColor() {
   let o = Math.round;
@@ -35,6 +40,7 @@ function getRandomColor() {
   return "rgb(" + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
 }
 
+//choose between grayscale and rgb
 function setColor(event){
   if (color == 'gray'){
     event.target.style.backgroundColor = 'gray';
@@ -45,34 +51,33 @@ function setColor(event){
   }
 }
 
-// grayscaleMode button
-grayscaleMode.classList.add('active');
-grayscaleMode.addEventListener('click', function(){
-  rainbowMode.classList.remove('active');
-  clearGrid.classList.remove('active');
-  grayscaleMode.classList.add('active');
+//grayscaleMode button
+grayscaleMode.addEventListener('click', (e)=>{
+  audio.play();
   color = 'gray';
 });
 
-// rainbowMode button
-rainbowMode.addEventListener('click', function(){
-  clearGrid.classList.remove('active');
-  grayscaleMode.classList.remove('active');
-  rainbowMode.classList.add('active');
+//rainbowMode button
+rainbowMode.addEventListener('click', (e)=>{
+  audio.play();
   color = 'rgb';
 });
 
-// clearGrid button
+//clearGrid button
 clearGrid.addEventListener("click", (e)=>{
+  audio.play();
   grid.innerHTML = "";
   createGrid(size);
 });
 
-// resizeGrid button
+//resizeGrid button
 resizeGrid.addEventListener('click', (e)=> {
+  audio.play();
   grid.innerHTML = "";
-  size = parseInt(prompt('What is the size of the grid?'))
+  size = parseInt(prompt('Enter Grid Size (Max of 100):'));
+  if (size > 100) {
+    alert("ERROR! Grid Size larger than 100. Please try again.");
+    return;
+  }
   createGrid(size);
-})
-
-createGrid(size);
+});
